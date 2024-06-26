ce71d62a5dc4f7e7bd0e91c580d4e437bc06dcaa
David Adriazola <d.adriazolamuriel@gmail.com>
2024-06-26 17:52:33 -0400

    step1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from '../app/domains/products/pages/list/list.component'
+import { AboutComponent } from '../app/domains/info/pages/about/about.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+
+];
